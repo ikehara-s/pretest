e82f7ef4814da4a960f6789e0ff60755466dac5f
ikehara-s <67934806+ikehara-s@users.noreply.github.com>
2020-07-25 01:40:45 +0900

    Update Oracle_Practice.js

--- Oracle_Practice.js
+++ Oracle_Practice.js
@@ -97,8 +97,7 @@ function sortQuestion(){
 	// 1
 	//=============================================================================
 	q_list.push(new Question('しきい値、メトリック、サーバー生成アラートについて正しい説明を3つ選択しなさい。',
-	'データベース・パフォーマンスの監視と調整'
-	+ '\n★★★★☆'));
+	'));
 	pushChoice('すべてのメトリックはインスタンスに関連付けられている。', false);
 	pushChoice('クリアされたステートフルアラートは、DBA_ALERT_HISTORYコマンドを実行することで表示されます。', true);
 	pushChoice('空き領域のアラートは、根本的な問題が解決されると自動的にクリアされます。', true);
@@ -235,6 +234,147 @@ function sortQuestion(){
 	pushChoice('シングルバイト文字セットは常に7ビットのコード化スキームを使用します。', false);
 	pushChoice('マルチバイト文字セットを使用すると、シングルバイト文字セットよりもスペースを効率的に使用できます。', false);
 	pushChoice('シングルバイト文字セットは常に8ビットのコード化スキームを使用します。', false);
+	sortChoice();
+	
+	//=============================================================================
+	// 12
+	//=============================================================================
+	q_list.push(new Question('セッションとサービスのモニタリング待機について正しい説明を3つ選択しなさい。',
+	''));
+	pushChoice('V$SESSION_EVENTは、セッションで少なくとも1回待機が発生した場合、過去および既存のすべてのセッションの待機をすべて表示する。', false);
+	pushChoice('V$SERVICE_EVENTは、サービスに対して少なくとも1回待機が発生した場合、すべてのサービスのすべての待機を表示する。', true);
+	pushChoice('V$SESSION_WAIT_CLASSは、待機中のセッションについてのみ、待機クラスごとに分類された待機を表示する。', true);
+	pushChoice('V$SESSION_WAITとV$SESSIONの両方に、待機していないセッションが最後に待機したイベントの詳細が含まれている。', false);
+	pushChoice('V$SESSION_EVENTは、セッションで少なくとも1回待機が発生した場合、過去のすべてのセッションの待機をすべて表示する。', false);
+	pushChoice('V$SESSION_WAITとV$SESSIONの両方に、セッションが現在待機しているイベントの詳細が含まれている。', true);
+	sortChoice();
+
+	//=============================================================================
+	// 13
+	//=============================================================================
+	q_list.push(new Question('UNIVERSITYテーブルスペースを1つのデータベースから別のデータベースに転送する必要があります。'
+	+ '\nUNIVERSITYテーブルスペースは現在読み取り/書き込みで開いています。'
+	+ '\nソースと宛先のプラットフォームでは、エンディアン形式が異なります。'
+	+ '\nこのアクションのリストを調べます。'
+	+ '\n'
+	+ '\n1. ソースシステムでUNIVERSITYテーブルスペースを読み取り専用にします。'
+	+ '\n2. EXPDPを使用してUNIVERSITYテーブルスペースメタデータをエクスポートします。'
+	+ '\n3. ソースシステムでRMANを使用して、UNIVERSITYテーブルスペースデータファイルを宛先プラットフォームフォーマットに変換します。'
+	+ '\n4. UNIVERSITYテーブルスペースデータファイルを宛先システムにコピーします。'
+	+ '\n5. Data Pumpダンプ・セットを宛先システムにコピーします。'
+	+ '\n6. 宛先システムでRMANを使用して、UNIVERSITYテーブルスペースデータファイルを宛先プラットフォーム形式に変換します。'
+	+ '\n7. IMPDPを使用してUNIVERSITYテーブルスペースメタデータをインポートします。'
+	+ '\n8. 宛先システムでUNIVERSITYテーブルスペースを読み取り/書き込み可能にします。'
+	+ '\n'
+	+ '\nUNIVERSITYテーブルスペースをトランスポートするために必要なアクションの最小数は、正しい順序でどれですか。',
+	''));
+	pushChoice('1, 2, 4, 5, 7, 8', false);
+	pushChoice('1, 2, 4, 6, 7, 8', false);
+	pushChoice('1, 2, 3, 4, 5, 7, 8', true);
+	pushChoice('1, 2, 3, 4, 5, 6, 7, 8', false);
+	pushChoice('2, 4, 5, 6, 7', false);
+	sortChoice();
+
+	//=============================================================================
+	// 14
+	//=============================================================================
+	q_list.push(new Question('Oracle Grid InfrastructureとOracle Relational Database Management System（RDBMS）のOSグループとユーザーについて正しい説明を2つ選択しなさい。',
+	''));
+	pushChoice('デフォルトでは、OSASMグループのメンバーは自動ストレージ管理およびRDBMSインスタンスにアクセスできます。', false);
+	pushChoice('Oracle Grid InfrastructureおよびOracle Databaseの所有者のプライマリ・グループは、Oracle Inventoryグループである必要があります。', true);
+	pushChoice('Oracle Grid Infrastructureインストールは、グリッド・ユーザーが所有する必要があります。', false);
+	pushChoice('Oracle Grid Infrastructureの所有者は、Oracle RestartおよびOracle自動ストレージ管理バイナリを所有しています。', true);
+	pushChoice('Oracle Grid Infrastructureの所有者は、セカンダリグループとしてOSOPER、OSBACKUPDBA、およびOSKMDBAを持っている必要があります。', false);
+	pushChoice('自動ストレージ管理とOracleデータベースには同じOSDBAグループを使用する必要があります。', false);
+	sortChoice();
+
+	//=============================================================================
+	// 15
+	//=============================================================================
+	q_list.push(new Question('Recovery Manager（RMAN）を使用したデータベースの複製について正しい説明を4つ選択しなさい。',
+	''));
+	pushChoice('複製は、補助データベースインスタンスがターゲットデータベースインスタンスからバックアップセットをプルすることで実行できます。', true);
+	pushChoice('補助インスタンスへの接続は常に必要です。', true);
+	pushChoice('ターゲットデータベースのサブセットを複製できます。', true);
+	pushChoice('複製されたデータベースには、常に新しいDBIDが作成されます。', false);
+	pushChoice('リカバリカタログインスタンスへの接続は常に必要です。', false);
+	pushChoice('ターゲットデータベースのバックアップは常に必要です。', false);
+	pushChoice('複製は、ターゲットデータベースインスタンスがコピーを補助データベースインスタンスにプッシュさせることで実行できます。', true);
+	pushChoice('ターゲットデータベースインスタンスへの接続は常に必要です。', false);
+	sortChoice();
+
+	//=============================================================================
+	// 16
+	//=============================================================================
+	q_list.push(new Question('CDB1と呼ばれるコンテナーデータベースはOMF対応です。'
+	+ '\nPDB_FILE_NAME_CONVERTパラメータはCDB1で構成されていません。'
+	+ '\nPDB1は、週の初めにCDB1から切り離されました。'
+	+ '\nCDB1で実行されるこのコマンドを調べます。'
+	+ '\n'
+	+ '\nSQL> CREATE PLUGGABLE DATABASE pdb1 USING "/u01/app/oracle/oradata/pdb1.xml"'
+	+ '\n  2  SOURCE_FILE_NAME_CONVERT = ("/u01/app/oracle/oradata/", "/u02/app/oracle/oradata/");'
+	+ '\n'
+	+ '\n正しい説明を2つ選択しなさい。',
+	''));
+	pushChoice('PDB1データファイルはすでに正しい場所に存在しています。', false);
+	pushChoice('コマンドを実行する前に、DBMS_PDB.CHECK_PLUG_COMPATIBILITYをCDB1で実行する必要があります。', false);
+	pushChoice('コマンドを実行する前に、PDB_FILE_NAME_CONVERTを設定する必要があります。', false);
+	pushChoice('"/u01/app/oracle/oradata/pdb1.xml"には、PDB1のデータファイルの現在の場所が含まれていません。', true);
+	pushChoice('PDB1はCDB1から削除する必要があります。', true);
+	sortChoice();
+
+	//=============================================================================
+	// 17
+	//=============================================================================
+	q_list.push(new Question('Recovery Manager（RMAN）のイメージコピーを使用してプラットフォーム間でデータベースを転送することについて正しい説明を3つ選択しなさい。',
+	'答えは3つ'));
+	pushChoice('デフォルトでは、転送されたデータベースはOracle Managed Files（OMF）を使用します。', true);
+	pushChoice('データファイルは宛先システムで変換できます。', true);
+	pushChoice('データファイルはソースシステムで変換できます。', true);
+	pushChoice('転送されたデータベース用に新しいDBIDが自動的に作成されます。', false);
+	pushChoice('データベースは、エンディアン形式が異なるシステム間で転送できます。', true);
+	pushChoice('パスワードファイルはRMANによって自動的に変換されます。', false);
+	sortChoice();
+
+	//=============================================================================
+	// 18
+	//=============================================================================
+	q_list.push(new Question('このコマンドについて調べます。'
+	+ '\n'
+	+ '\n$ rhpctl move database -sourcehome Oracle_home_path -destinationhome Oracle_home_path'
+	+ '\n'
+	+ '\nこのコマンドを使用する目的として正しい説明を2つ選択しなさい。',
+	''));
+	pushChoice('既存のOracle Databaseホームを同じサーバー上のOracleソフトウェアの新しいリリースに切り替える。', false);
+	pushChoice('読み取り専用のOracleホームに切り替える。', false);
+	pushChoice('ロールバック操作の一部として前のOracleホームに戻る。', true);
+	pushChoice('集中型Rapid Home Provisioningサーバーを使用しているときにOracle Databaseホームを切り替える。', false);
+	pushChoice('パッチが適用されたOracle Databaseホームに切り替える。', true);
+	sortChoice();
+
+	//=============================================================================
+	// 19
+	//=============================================================================
+	q_list.push(new Question('CDBでLOCAL_UNDO_ENABLEDプロパティをfalseに変更することについて正しい説明を2つ選択しなさい。',
+	''));
+	pushChoice('変更後、必要な権限を持つ共通ユーザーのみが、CDB＆ROOTにUNDO表領域を作成できます。', true);
+	pushChoice('新しいPDBと既存のPDBは、CDB$ROOTのデフォルトのUNDO表領域を使用するように自動的に構成されます。', true);
+	pushChoice('変更後、CDB$ROOTに存在できるUNDO表領域は1つだけです。', false);
+	pushChoice('変更後、必要な権限を持つすべてのユーザーがPDBにUNDO表領域を作成できます。', false);
+	pushChoice('PDBに存在するUNDO表領域は、変更前に削除する必要があります。', false);
+	pushChoice('変更後、新しいUNDOモードを有効にするには、既存の各PDBを再度開く必要があります。', false);
+	sortChoice();
+
+	//=============================================================================
+	// 20
+	//=============================================================================
+	q_list.push(new Question('SQL Performance Analyzerについて正しい説明を2つ選択しなさい。',
+	'答えは2つ'));
+	pushChoice('SQL Access Advisorと統合されています。', false);
+	pushChoice('SQLワークロードの応答時間に対するシステム変更の影響を予測します。', true);
+	pushChoice('分析タスクの各SQLステートメントの実行前後の統計を提供します。', true);
+	pushChoice('分析タスク内のすべてのSQLステートメントをグループとして詳細に分析できます。', true);
+	pushChoice('最初に同時に実行されたSQLステートメントは、SPAによって同時に実行されます。', false);
 	sortChoice();
 }());
 
